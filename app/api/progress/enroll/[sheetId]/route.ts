db02cf3cad2aa52d30e6eb7b127f5342f890c382
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-03-24 06:19:21 +0530

    add enroll to sheet route

---
+++ app/api/progress/enroll/[sheetId]/route.ts
@@ -0,0 +1,94 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import topicModel from "@/model/topic.model";
+import SheetModel from "@/model/sheet.model";
+import userSheetProgressModel from "@/model/userSheetProgress.model";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+export async function POST(
+  request: Request,
+  { params }: { params: { sheetId: string } },
+) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  try {
+    await dbConnect();
+
+    const { sheetId } = params;
+    if (!sheetId) {
+      return Response.json(
+        {
+          success: false,
+          message: "No sheetId found",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+    const sheet = await SheetModel.findById(sheetId);
+    if (!sheet) {
+      return Response.json(
+        { success: false, message: "No sheet found" },
+        {
+          status: 400,
+        },
+      );
+    }
+    const existingUserSheetProgress = await userSheetProgressModel.findOne({
+      userId: session.user._id,
+      sheetId,
+    });
+    if (existingUserSheetProgress) {
+      return Response.json(
+        {
+          success: false,
+          message: "Already enrolled",
+        },
+        {
+          status: 200,
+        },
+      );
+    }
+    const totalTopics = await topicModel.countDocuments({ sheetId });
+    await userSheetProgressModel.create({
+      userId: session.user._id,
+      sheetId,
+      enrolledAt: new Date(),
+      totalTopics,
+      completedTopics: 0,
+      progressPercent: 0,
+      subjectProgress: [],
+    });
+    return Response.json(
+      {
+        success: false,
+        message: "Enrolled successfully in the sheet",
+      },
+      {
+        status: 201,
+      },
+    );
+  } catch (error) {
+    console.error("Error enrolling to the sheet", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error enrolling to the audience",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+}
