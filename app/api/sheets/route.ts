808f361dd891a97a32cd83188b0e6ec2a2fbb8a3
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-04-10 14:26:05 +0530

    add fetching all sheets model

--- app/api/sheets/route.ts
+++ app/api/sheets/route.ts
@@ -1,5 +1,37 @@
 import UserModel from "@/model/user.model";
-import UserTopicProgressModel from "@/model/userTopicProgress.model";
-import userSheetProgressModel from "@/model/userSheetProgress.model";
+import SheetModel from "@/model/sheet.model";
 import dbConnect from "@/lib/dbConnect";
-import { getServerSession } from "next-auth";
+import { success } from "zod";
+
+export async function GET(request: Request) {
+  try {
+    await dbConnect();
+    const sheets = await SheetModel.find({});
+    if (!sheets) {
+      return Response.json(
+        { success: false, message: "Sheets not found" },
+        {
+          status: 404,
+        },
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Sheets fetched Successfully",
+        data: sheets,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err: any) {
+    console.error("Error fetching the sheets", err);
+    return Response.json(
+      { success: false, message: err.message },
+      {
+        status: 400,
+      },
+    );
+  }
+}
