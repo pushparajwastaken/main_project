593e13cae2d8f716764fd7d774e6fcf7f67a4cc2
Pushparaj <psinghparmar42@gmail.com>
2026-06-19 23:44:26 +0530

    full removal

--- schemas/forgotPasswordSchema.ts
+++
@@ -1,4 +0,0 @@
-import z from "zod";
-export const forgotPasswordSchema = z.object({
-  email: z.email("Invalid email address"),
-});
--- schemas/loginSchema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const LoginSchema = z.object({
-  identifier: z.string().min(1, "Email or username is required"),
-  password: z.string().min(1, "Password is required"),
-});
